c30eeb62bb593e371528937b0b6a13b402b1367d
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2024-06-11 12:25:23 +0300

    contex & questions

--- Do-You-See-Sharp/Context.cs
+++ Do-You-See-Sharp/Context.cs
@@ -2,7 +2,62 @@
 
 public class Context
 {
-	public Context()
+	private string _storyText;
+	private List<Question> _questions;
+	private int _currentQuestionIndex;
+	private List<People> _people;
+
+	public Context(string storyText)
 	{
+		_storyText = storyText;
+		_questions = new List<Question>();
+		_people = new List<People>();
+		_currentQuestionIndex = 0;
+	}
+
+	public void AddNewQuestion(Question question)
+	{
+		_questions.Add(question);
+	}
+
+	public void AddNewPeople(People people)
+	{
+		_people.Add(people);
+	}
+
+	public string GetStory()
+	{
+
+		return _storyText; 
+	}
+	public string GetCurrentQuestion()
+	{
+		if (_questions.Count > 0)
+		{
+			return _questions[_currentQuestionIndex].GetQuestionText();
+
+		}
+		else
+		{
+			return "THERE IS NO QUESTION";
+		}
+	}
+	public bool TryToAnswerToCurrentQuestion(string answer)
+	{
+
+		return answer == _questions[_currentQuestionIndex].GetAnswerText();
+
+	}
+	public bool MoveToNextQuestionIfAvailable()
+	{
+		
+		if (_questions.Count - 1 > _currentQuestionIndex)
+		{
+			_currentQuestionIndex++;
+			return true;
+
+		}
+		return false;
+
 	}
 }
---
+++ Do-You-See-Sharp/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Question
+{
+
+    private string _questionText;
+
+    private string _questionAnswer;
+
+    public Question(string questionText, string questionAnswer)
+    {
+        _questionText = questionText;
+        _questionAnswer = questionAnswer;
+    }
+
+    public string GetQuestionText()
+    {
+
+        return _questionText;
+    }
+
+    public string GetAnswerText()
+    {
+
+        return _questionAnswer;
+    }
+}
