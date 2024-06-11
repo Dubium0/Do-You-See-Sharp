a1adc4ee3e5de12fb7c95ba271b3c121f3eacf08
Dilara MANDIRACI <dilara.mandiraci@taleworlds.corp>
2024-06-11 11:55:31 +0300

    People.cs created

--- Do-You-See-Sharp/People.cs
+++ Do-You-See-Sharp/People.cs
@@ -2,7 +2,52 @@
 
 public class People
 {
-	public People()
-	{
-	}
+    private string _info;
+    private string _initialClaim;
+    private string _extraHint;
+    private bool _isHintAcquired;
+
+    public People(string info, string initialClaim, string extraHint)
+    {
+        _info = info;
+        _initialClaim = initialClaim;
+        _extraHint = extraHint;
+        _isHintAcquired = false;
+    }
+
+    public string GetInfo()
+    {
+        return _info;
+    }
+
+    public void SetInfo(string value)
+    {
+        _info = value;
+    }
+
+    public string GetInitialClaim()
+    {
+        return _initialClaim;
+    }
+
+    public void SetInitialClaim(string value)
+    {
+        _initialClaim = value;
+    }
+
+    public string GetExtraHint()
+    {
+        _isHintAcquired = true;
+        return _extraHint;
+    }
+
+    public void SetExtraHint(string value)
+    {
+        _extraHint = value;
+    }
+
+    public bool IsHintAcquired()
+    {
+        return _isHintAcquired;
+    }
 }
--- Do-You-See-Sharp/deneme.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Class1
-{
-	public Class1()
-	{
-		//hellooo this is a trial...
-	}
-}
